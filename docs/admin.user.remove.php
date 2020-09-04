96ce7f77431717c831bca6a6e24e86e71d5d810c
cyhan <38914065+cyeehan@users.noreply.github.com>
2020-09-05 02:33:49 +0800

    Test

--- docs/admin.user.remove.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'connection.php';
-session_start();
-$user_id = $_GET['id'];
-//$users_id=$_SESSION['id'];
-$delete_query = "delete from users where users.id = '$user_id'";
-$delete_query_result = mysqli_query($con, $delete_query) or die(mysqli_error($con));
-header('location: admin.view.php');
-?>
